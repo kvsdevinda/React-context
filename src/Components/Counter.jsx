09c59de3683b39f48fea1f1af3e907576a2c1d39
kvsdevinda <kvsdevinda@gmail.com>
2025-01-21 16:11:04 +0530

    Update Counter.jsx

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Counter = ({ initialCount = 0 }) => {
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+  const restart = () => setCount(0);
+  const switchValues = () => setCount((prev) => prev * -1);
+
+  return (
+    <div>
+      <h1>
+        Count: <h3 data-testid="count">{count}</h3>
+      </h1>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={restart}>Restart</button>
+        <button onClick={switchValues}>Switch Values</button>
+      </div>
+    </div>
+  );
+};
+
+Counter.propTypes = {
+  initialCount: PropTypes.number,
+};
+
+export default Counter;
