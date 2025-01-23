e80f9ca144c7c80d7f1ee9d578db2f1baf3b0af6
Pasan47 <pasan.rathnayaka01@gmail.com>
2025-01-23 11:06:06 +0530

    all tests are passed

--- src/component.test.js
+++ src/component.test.js
@@ -17,3 +17,34 @@ test("count should Increment by one if the increment button is click" , () =>{
   const countvalue = Number(getByTestId("count").textContent);
   expect(countvalue).toBe(1);
 });
+
+
+test("count should Decrement by one if the Decrement button is click" , () =>{
+  const { getByTestId , getByRole } = render(<Counter initialCount={0} />);
+  const DecrementBtn = getByRole("button" , {name :"Decrement"});
+  fireEvent.click(DecrementBtn);
+  const countvalue = Number(getByTestId("count").textContent);
+  expect(countvalue).toBe(-1);
+});
+
+describe(Counter, () => {
+  it("count should restart by one if the restart button is click" , () =>{
+    const { getByTestId , getByRole } = render(<Counter initialCount={50} />);
+    const RestartBtn = getByRole("button" , {name :"Restart"});
+    expect(Number(getByTestId("count").textContent)).toEqual(50);
+    fireEvent.click(RestartBtn);
+    const countvalue = Number(getByTestId("count").textContent);
+    expect(countvalue).toBe(0);
+  });
+});
+
+describe(Counter, () => {
+  it("count should switchValues by one if the switchValues button is click" , () =>{
+    const { getByTestId , getByRole } = render(<Counter initialCount={50} />);
+    const switchValuesBtn = getByRole("button" , {name :"Switch Values"});
+    expect(Number(getByTestId("count").textContent)).toEqual(50);
+    fireEvent.click(switchValuesBtn);
+    const countvalue = Number(getByTestId("count").textContent);
+    expect(countvalue).toBe(-50);
+  });
+});
